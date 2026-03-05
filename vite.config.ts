94df34ebf9b3b6084099016111998d8a183778b0
bothumilde <suscripcionesalejo@gmail.com>
2026-03-05 15:36:20 -0500

    pezuña

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     sourcemap: false, // Disable sourcemap for production
     minify: 'terser',
     rollupOptions: {
